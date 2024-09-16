4035a63549366842b4c263a9f327e5a2ff516631
Abhinay <121sriramreddy@gmail.com>
2024-09-16 18:15:11 +0530

    created authentication middleware

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const mainrouter=require("./routes/index")
 const cors=require("cors")
 const app=express()
 app.use(cors)
 app.use(express.json())
+const mainrouter=require("./routes/index")
+
 
 
 app.use("/api/v1",mainrouter)
---
+++ backend/middleware.js
@@ -0,0 +1,23 @@
+const jwt=require("jsonwebtoken")
+const secret=require("./config")
+
+const auth_middleware=(req,res,next)=>{
+    let auth=req.headers.authorization
+    if(!auth || !auth.startsWith('Bearer')){
+        return res.status(403).json({})
+
+    }
+
+let token=auth.split(' ')[1]
+
+try{
+    const decoded=jwt.verify(token,secret)
+    req.userid=decoded.userid
+    next()
+}catch{
+res.status(403).json({})
+}
+
+}
+
+module.exports=auth_middleware
